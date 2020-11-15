94062bce3b9c776fba06114f16d776b99da790a8
lukas k <experimentalhypothesis@gmail.com>
2020-11-15 23:21:35 +0100

    regex matching

--- ArgParser.py
+++ ArgParser.py
@@ -1,21 +1,27 @@
 import argparse
 import NameNormalizer
 
-class ArgParser:
-    """ Class reponsible for parsing command line args. """
 
+class ArgParser:
+    """ Class responsible for parsing command line args. """
+
+    @staticmethod
     def _parseArgs():
         """ Prepare the parse args. """
         parser = argparse.ArgumentParser()
 
-        parser.add_argument("-strar", "--striptokenfromartist", action="store_true")
-        parser.add_argument("-stral", "--striptokenfromalbum", action="store_true")
-        parser.add_argument("-strso", "--striptokenfromsong", action="store_true")
+        parser.add_argument("-rnar", "--striptokenfromartist", action="store_true")
+        parser.add_argument("-rnal", "--striptokenfromalbum", action="store_true")
+        parser.add_argument("-rnso", "--striptokenfromsong", action="store_true")
+        parser.add_argument("-rgxar", "--stripregexfromartist", action="store_true")
+        parser.add_argument("-rgxal", "--stripregexfromalbum", action="store_true")
+        parser.add_argument("-rgxso", "--stripregexfromsong", action="store_true")
         parser.add_argument("-T", "--token", type=str, required=True)
         parser.add_argument("-R", "--root", type=str, required=True)
-        
+
         return parser.parse_args()
 
+    @staticmethod
     def run():
         """ Run function based on CLI arguments input. """
         args = ArgParser._parseArgs()
@@ -26,7 +32,13 @@ class ArgParser:
             NameNormalizer.NameNormalizer.stripTokenFromAlbumName(args.token, args.root)
         elif args.striptokenfromsong:
             NameNormalizer.NameNormalizer.stripTokenFromSongName(args.token, args.root)
-        
+        elif args.stripregexfromartist:
+            NameNormalizer.NameNormalizer.stripRegFromArtistName(args.token, args.root)
+        elif args.stripregexfromalbum:
+            NameNormalizer.NameNormalizer.stripRegFromAlbumName(args.token, args.root)
+        elif args.stripregexfromsong:
+            NameNormalizer.NameNormalizer.stripRegFromSongName(args.token, args.root)
+
 # if __name__ == "__main__":
 #     print("hello")
 #     ArgParser.runCommands()
--- NameNormalizer.py
+++ NameNormalizer.py
@@ -1,9 +1,7 @@
-import os, argparse
-
-import math
+import os, argparse, re
 
 class NameNormalizer:
-    
+
     # def __init__(self, root:str):
     #     """ Specify the root path """
     #     self.root = root
@@ -11,15 +9,17 @@ class NameNormalizer:
     # def __str__(self):
     #     return f"NameNormalizer - {self.root}"
 
+    @staticmethod
     def _stripToken(token, *args):
         """ Strip specified token from a filepath. """
         src = os.path.join(*args)
         dst = os.path.join(*args[0:-1], args[-1].replace(token, " "))
         if src == dst:
-            return 
+            return
         print(f"Stripping {token} from {src} -> {dst}")
         os.rename(src, dst)
 
+    @staticmethod
     def stripTokenFromArtistName(token, root) -> None:
         """ Deletes a specified token from all artist folder names. """
         for artist in os.listdir(root):
@@ -27,6 +27,7 @@ class NameNormalizer:
                 continue
             NameNormalizer._stripToken(token, root, artist)
 
+    @staticmethod
     def stripTokenFromAlbumName(token, root) -> None:
         """ Deletes a specified token from all artist folder names. """
         for artist in os.listdir(root):
@@ -37,16 +38,51 @@ class NameNormalizer:
                     continue
                 NameNormalizer._stripToken(token, root, artist, album)
 
+    @staticmethod
     def stripTokenFromSongName(token, root) -> None:
         """ Deletes a specified token or string from all songs names """
         for path, dirs, folders in os.walk(root):
             for file in folders:
                 NameNormalizer._stripToken(token, path, file)
 
+    @staticmethod
+    def _stripReg(regex, *args) -> None:
+        """ Strip regex expression from a filepath. """
+        src = os.path.join(*args)
+        dst = os.path.join(*args[0:1], re.sub(regex, " ", args[-1]))
+        if src == dst:
+            return
+        print(f"Striping Regex {regex} from {src} -> {dst}")
+        os.rename(src, dst)
+
+    @staticmethod
+    def stripRegFromArtistName(regex, root) -> None:
+        """ Deletes string that matches regex expression form artist name. """
+        for artist in os.listdir(root):
+            if not os.path.isdir(os.path.join(root, artist)):
+                continue
+            NameNormalizer._stripReg(regex, root, artist)
+
+    @staticmethod
+    def stripRegFromAlbumName(regex, root) -> None:
+        """ Deletes string that matches regex expression form album name. """
+        for artist in os.listdir(root):
+            if not os.path.isdir(os.path.join(root, artist)):
+                continue
+            for album in os.listdir(os.path.join(root, artist)):
+                if not os.path.isdir(os.path.join(root, artist, album)):
+                    continue
+                NameNormalizer._stripReg(regex, root, artist, album)
+
+    @staticmethod
+    def stripRegFromSongName(regex, root) -> None:
+        """ Deletes string that matches regex expression form song name. """
+        for path, dirs, folders in os.walk(root):
+            for file in folders:
+                NameNormalizer._stripReg(regex, path, file)
+
+
+
 
 # if __name__ == "__main__":
 #     runCommands()
-
-
-
-

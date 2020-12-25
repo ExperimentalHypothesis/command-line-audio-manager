126849f3aa19a323e2b2c4f01b9e7b8b45e7c689
lukas k <experimentalhypothesis@gmail.com>
2020-12-25 12:10:32 +0100

    fix listing to terminal with coloring

--- NameChecker.py
+++ NameChecker.py
@@ -1,7 +1,8 @@
 
 import os, re
 from termcolor import colored
-
+from colorama import init
+init()  # to make coloring works on windows
 
 ## THIS CLASS IS USED AS THIRD
 class NameChecker:
@@ -28,6 +29,8 @@ class NameChecker:
     def listAllAlbums(root):
         """ Print all Albums folders to the console. Albums should be at second level, if at this level is file, it will be printed in red. """
         for artist in os.listdir(root):
+            if not os.path.isdir(os.path.join(root, artist)):
+                continue
             for album in os.listdir(os.path.join(root, artist)):
                 if os.path.isdir(os.path.join(root, artist, album)):
                     print(album)
@@ -38,6 +41,8 @@ class NameChecker:
     def listAllSongs(root):
         """ Print all Songs to the console. Songs should be at third level, if at this level is album (directory), it will be printed in red. """
         for artist in os.listdir(root):
+            if not os.path.isdir(os.path.join(root, artist)):
+                continue
             for album in os.listdir(os.path.join(root, artist)):
                 if not os.path.isdir(os.path.join(root, artist, album)):
                     continue
